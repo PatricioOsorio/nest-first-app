409e8a16afe75b3e2f929bd98f36ac874590fdc9
Patricio Osorio <patriciomiguel_12@hotmail.com>
2026-08-22 19:17:24 -0600

    feat: 1.1 bootstrap app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { envSchema } from '@/core/env.schema';
+import { HealthModule } from '@/core/health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (config) => envSchema.parse(config),
     }),
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
